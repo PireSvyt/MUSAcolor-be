ca802b90d7e1e0117ef7c077752eeeeb99d708d0
Pire Svyt <savoyat.p@gmail.com>
2024-01-19 07:09:22 +0100

    user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const authAuthenticate = require("../controllers/auth/authAuthenticate.js");
 const adminAuthenticate = require("../controllers/admin/adminAuthenticate.js");
+const authAuthenticateAsPractician = require("../controllers/auth/authAuthenticateAsPractician.js");
 
 const userCreate = require("../controllers/user/userCreate.js");
 const userSave = require("../controllers/user/userSave.js");
@@ -13,8 +14,9 @@ const userDelete = require("../controllers/user/userDelete.js");
 
 router.post("/v1/create", authAuthenticate, adminAuthenticate, userCreate);
 router.post("/v1/save", authAuthenticate, adminAuthenticate, userSave);
-router.get("/v1", authAuthenticate, adminAuthenticate, userGetMe);
-router.get("/v1", authAuthenticate, adminAuthenticate, userGetAll);
+router.get("/v1/all", authAuthenticate, adminAuthenticate, userGetAll);
+router.get("/v1/:userid", authAuthenticate, adminAuthenticate, userGetOne);
+router.get("/v1", authAuthenticate, authAuthenticateAsPractician, userGetMe);
 router.delete("/v1/:userid", authAuthenticate, adminAuthenticate, userDelete);
 
 module.exports = router;
