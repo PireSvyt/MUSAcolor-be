330b2c7a4d17208d47b298bc3d4aa896293fb8ca
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2024-09-11 12:26:14 +0200

    Create examGetRemotely.js

---
+++ src/controllers/exam/examGetRemotely.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+const Exam = require("../../models/Exam.js");
+
+module.exports = examGetRemotely = (req, res, next) => {
+  /*
+  
+  sends back the exam to be performed if not yet performed
+  
+  possible response types
+  * exam.getremotely.success
+  * exam.getremotely.alreadyperformed
+  * exam.getremotely.error.denied
+  * exam.getremotely.error.notfound
+  
+  */
+
+  if (process.env.DEBUG) {
+    console.log("exam.getremotely");
+  }
+
+  Exam.findOne({ token: req.params.examtoken })
+    .then((exam) => {
+      if (exam !== undefined) {
+        if (exam.results !== undefined) {
+          console.log("exam.getremotely.alreadyperformed");
+          return res.status(401).json({
+            type: "exam.getremotely.alreadyperformed",
+            data: {
+              exam: undefined,
+            },
+          });          
+        }        
+        // Cleaning prior sending to patient
+        let examToSend = {...exam}
+        const keysToSend = [ "schema", "examid", "type"]
+        Object.keys(exam).forEach(k => {
+          if (!keysToSend.includes()) {
+            delete examToSend[k]            
+          }
+        })        
+        console.log("exam.getremotely.success");
+        return res.status(200).json({
+          type: "exam.getremotely.success",
+          data: {
+            exam: examToSend,
+          },
+        });
+      } else {
+        console.log("exam.getremotely.error.denied");
+        return res.status(403).json({
+          type: "exam.getremotely.error.denied",
+          data: {
+            exam: undefined,
+          },
+        });
+      }
+    })
+    .catch((error) => {
+      console.log("exam.getremotely.error.onfind");
+      console.error(error);
+      return res.status(400).json({
+        type: "exam.getremotely.error.onfind",
+        error: error,
+        data: {
+          exam: undefined,
+        },
+      });
+    });
+};
